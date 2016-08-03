d5da3fcf4ef96c45aa47385a9f8e03d98074a997
Aleksei <alekseylisovich@gmail.com>
2016-08-03 10:29:41 +0300

    dialog window

--- scripts/knockoutBehavior.js
+++ scripts/knockoutBehavior.js
@@ -12,22 +12,27 @@ function SeatReservation(initialMeal) {
 function ReservationsViewModel() {
     var self = this;
     self.totalPrice = ko.observable(0);
+
+    self.formattedTotalPrice = ko.computed(function() {
+        var price = self.totalPrice();
+        return price.toFixed(2);
+    });
     self.availableMeals = [
-        { mealName: "Картофель с ризото", photo: "<img src='photo/PotatoesWithChorizo.jpg'> ", price: 99.99, available: "есть" },
-        { mealName: "Картошка с тушенкой", photo: "<img src='photo/КАРТОШКА_С_ТУШЕНКОЙ.jpg'>", price: 149.99, available: "есть" },
-        { mealName: "Котлетки на подушке из цукини", photo: "<img src='photo/КОТЛЕТКИ_НА_ПОДУШКЕ_ИЗ_ЦУКИНИ.jpg'>", price: 179.99, available: "есть" },
-        { mealName: "Куриные крылышки", photo: "<img src='photo/КУРИНЫЕ_КРЫЛЫШКИ.jpg'>", price: 74.99, available: "есть" },
-        { mealName: "Куриные шашлыки", photo: "<img src='photo/КУРИНЫЕ_ШАШЛЫЧКИ_SATAY.jpg'>", price: 59.99, available: "есть" },
-        { mealName: "Курица в соляно-апельсиновом маринаде", photo: "<img src='photo/КУРИЦА_В_СОЛЯНО-АПЕЛЬСИНОВОМ_МАРИНАДЕ.jpg'>", price: 99.99, available: "есть" },
-        { mealName: "Макароны с мясом по-грузински", photo: "<img src='photo/МАКАРОНЫ_С_МЯСОМ_ПО-ГРУЗИНСКИ.jpg'>", price: 59.99, available: "нет" },
-        { mealName: "Паста с семгой и креветками", photo: "<img src='photo/ПАСТА_С_СЕМГОЙ_И_КРЕВЕТКАМИ.jpg'>", price: 0, available: "есть" },
-        { mealName: "Ризотто с помидорами", photo: "<img src='photo/РИЗОТТО_С_ПОМИДОРАМИ_И_БАЗИЛИКОМ.jpg'>", price: 0, available: "есть" },
-        { mealName: "Рисовая бабка", photo: "<img src='photo/РИСОВАЯ_БАБКА.jpg'>", price: 0, available: "есть" },
-        { mealName: "Скумбрия запеченная", photo: "<img src='photo/СКУМБРИЯ_ЗАПЕЧЕННАЯ_С_ПАПРИКОЙ_И_ЧЕСНОКОМ.jpg'>", price: 0, available: "есть" },
-        { mealName: "Спагетти с памидорами", photo: "<img src='photo/СПАГЕТТИ_СО_СВЕЖИМИ_ПОМИДОРАМИ.jpg'>", price: 0, available: "есть" },
-        { mealName: "Судак с лимоном и луком", photo: "<img src='photo/судак_с_лимоном_и_луком.jpg'>", price: 0, available: "есть" },
-        { mealName: "Тушенная картошка", photo: "<img src='photo/ТУШЕНАЯ_КАРТОШКА_В_МУЛЬТИВАРКЕ_С_КУРИНЫМ_ФИЛЕ.jpg'>", price: 0, available: "есть" },
-        { mealName: "Фрикадельки", photo: "<img src='photo/ФРИКАДЕЛЬКИ_ИЗ_ИНДЮШКИЕ.jpg'>", price: 0, available: "есть" }
+        { mealName: "Картофель с ризото", photo: "<img src='photo/dish1.jpg'> ", price: 9.99, available: "есть" },
+        { mealName: "Картошка с тушенкой", photo: "<img src='photo/dish2.jpg'>", price: 19.99, available: "есть" },
+        { mealName: "Котлетки на подушке из цукини", photo: "<img src='photo/dish3.jpg'>", price: 19.99, available: "есть" },
+        { mealName: "Куриные крылышки", photo: "<img src='photo/dish4.jpg'>", price: 7.99, available: "есть" },
+        { mealName: "Куриные шашлыки", photo: "<img src='photo/dish5.jpg'>", price: 5.99, available: "есть" },
+        { mealName: "Курица в соляно-апельсиновом маринаде", photo: "<img src='photo/dish6.jpg'>", price: 9.99, available: "есть" },
+        { mealName: "Макароны с мясом по-грузински", photo: "<img src='photo/dish7.jpg'>", price: 5.99, available: "нет" },
+        { mealName: "Паста с семгой и креветками", photo: "<img src='photo/dish8.jpg'>", price: 4.49, available: "есть" },
+        { mealName: "Ризотто с помидорами", photo: "<img src='photo/dish9.jpg'>", price: 0, available: "нет" },
+        { mealName: "Рисовая бабка", photo: "<img src='photo/dish10.jpg'>", price: 9.99, available: "есть" },
+        { mealName: "Скумбрия запеченная", photo: "<img src='photo/dish11.jpg'>", price: 0, available: "нет" },
+        { mealName: "Спагетти с памидорами", photo: "<img src='photo/dish12.jpg'>", price: 12.49, available: "есть" },
+        { mealName: "Судак с лимоном и луком", photo: "<img src='photo/dish13.jpg'>", price: 19.99, available: "есть" },
+        { mealName: "Тушенная картошка", photo: "<img src='photo/Тdish14.jpg'>", price: 24.99, available: "есть" },
+        { mealName: "Фрикадельки", photo: "<img src='photo/dish15.jpg'>", price: 5.99, available: "есть" }
     ];
 
     self.seats = ko.observableArray([
@@ -42,7 +47,7 @@ function ReservationsViewModel() {
 
     self.addPrice = function(data) {
         var count = +data.dishCount() * data.meal().price;
-        self.totalPrice(self.totalPrice() + count);
+        var r = self.totalPrice(self.totalPrice() + count);
         data.dishCount(data.dishCount() + 1);
     }
 }
