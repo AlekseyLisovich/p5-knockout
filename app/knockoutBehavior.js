ab01e48c7f70a02ea58a50cb42a217da42d11624
Aleksei <alekseylisovich@gmail.com>
2016-08-04 14:16:15 +0300

    menu confirmed edit

--- app/knockoutBehavior.js
+++ app/knockoutBehavior.js
@@ -2,7 +2,7 @@
 function SeatReservation(initialMeal) {
     var self = this;
     self.meal = ko.observable(initialMeal);
-    self.dishCount = ko.observable(1);
+    self.dishCount = ko.observable(0);
     self.availabity = ko.computed(function() {
         var available = self.meal().available;
         return available ? "Есть" : "Нет в наличии";
@@ -17,52 +17,56 @@ function ReservationsViewModel() {
         var price = self.totalPrice();
         return price.toFixed(2);
     });
+
+    self.selectedFood = ko.observableArray();
+
     self.availableMeals = [
-        { mealName: "Картофель с ризото", photo: "dish1.jpg", price: 9.99, available: true },
-        { mealName: "Картошка с тушенкой", photo: "dish2.jpg", price: 19.99, available: true },
-        { mealName: "Котлетки на подушке из цукини", photo: "dish3.jpg", price: 19.99, available: true },
-        { mealName: "Куриные крылышки", photo: "dish4.jpg", price: 5.99, available: true },
-        { mealName: "Куриные шашлыки", photo: "dish5.jpg", price: 5.99, available: true },
-        { mealName: "Курица в соляно-апельсиновом маринаде", photo: "dish6.jpg", price: 9.99, available: true },
-        { mealName: "Макароны с мясом по-грузински", photo: "dish7.jpg", price: 5.99, available: true },
-        { mealName: "Паста с семгой и креветками", photo: "dish8.jpg", price: 4.49, available: true },
-        { mealName: "Ризотто с помидорами", photo: "dish9.jpg", price: 5.99, available: false },
-        { mealName: "Рисовая бабка", photo: "dish10.jpg", price: 9.99, available: true },
-        { mealName: "Скумбрия запеченная", photo: "dish11.jpg", price: 0, available: false },
-        { mealName: "Спагетти с памидорами", photo: "dish12.jpg", price: 12.49, available: true },
-        { mealName: "Судак с лимоном и луком", photo: "dish13.jpg", price: 19.99, available: true },
-        { mealName: "Тушенная картошка", photo: "dish14.jpg", price: 24.99, available: true },
-        { mealName: "Фрикадельки", photo: "dish15.jpg", price: 5.99, available: true }
+        { mealName: "Картофель с ризото", photo: "dish1.jpg", price: 9.99, priceCounter: 0, available: true, id: 1 },
+        { mealName: "Картошка с тушенкой", photo: "dish2.jpg", price: 19.99,  priceCounter: 0, available: true, id: 2 },
+        { mealName: "Котлетки на подушке из цукини", photo: "dish3.jpg", price: 19.99,  priceCounter: 0, available: true, id: 3 },
+        { mealName: "Куриные крылышки", photo: "dish4.jpg", price: 5.99,  priceCounter: 0, available: true, id: 4 },
+        { mealName: "Куриные шашлыки", photo: "dish5.jpg", price: 5.99, priceCounter: 0, available: true, id: 5 },
+        { mealName: "Курица в соляно-апельсиновом маринаде", photo: "dish6.jpg", priceCounter: 0, price: 9.99, available: true, id: 6 },
+        { mealName: "Макароны с мясом по-грузински", photo: "dish7.jpg", priceCounter: 0, price: 5.99, available: true, id: 7 },
+        { mealName: "Паста с семгой и креветками", photo: "dish8.jpg", priceCounter: 0, price: 4.49, available: true, id: 8 },
+        { mealName: "Ризотто с помидорами", photo: "dish9.jpg", price: 5.99, priceCounter: 0, available: false, id: 9 },
+        { mealName: "Рисовая бабка", photo: "dish10.jpg", price: 9.99, priceCounter: 0, available: true, id: 10 },
+        { mealName: "Скумбрия запеченная", photo: "dish11.jpg", price: 0, priceCounter: 0, available: false, id: 11 },
+        { mealName: "Спагетти с памидорами", photo: "dish12.jpg", price: 12.49, priceCounter: 0, available: true, id: 12 },
+        { mealName: "Судак с лимоном и луком", photo: "dish13.jpg", price: 19.99, priceCounter: 0, available: true, id: 13 },
+        { mealName: "Тушенная картошка", photo: "dish14.jpg", price: 24.99, priceCounter: 0, available: true, id: 14 },
+        { mealName: "Фрикадельки", photo: "dish15.jpg", price: 5.99, priceCounter: 0, available: true, id: 15 }
     ];
 
-    self.seats = ko.observableArray([
-        new SeatReservation(self.availableMeals[0]),
-        new SeatReservation(self.availableMeals[1]),
-        new SeatReservation(self.availableMeals[2]),
-        new SeatReservation(self.availableMeals[3]),
-        new SeatReservation(self.availableMeals[4]),
-        new SeatReservation(self.availableMeals[5]),
-        new SeatReservation(self.availableMeals[6]),
-        new SeatReservation(self.availableMeals[7]),
-        new SeatReservation(self.availableMeals[8]),
-        new SeatReservation(self.availableMeals[9]),
-        new SeatReservation(self.availableMeals[10]),
-        new SeatReservation(self.availableMeals[11]),
-        new SeatReservation(self.availableMeals[12]),
-        new SeatReservation(self.availableMeals[13]),
-        new SeatReservation(self.availableMeals[14])
-    ]);
+    var food = [];
+    self.availableMeals.forEach(function(meal) {
+        food.push(new SeatReservation(meal));
+    })
+
+    self.foods = ko.observableArray(food);
+
+    self.removeFood = function(data, event) {
+        self.selectedFood.remove(data);
+        self.totalPrice(self.totalPrice() - data.meal().price);
+    };
 
     self.addPrice = function(data) {
-    if (data.dishCount() < 20) {
-        data.dishCount(+data.dishCount() + 1);
-        var count = +data.dishCount() * data.meal().price;
-        var r = self.totalPrice(self.totalPrice() + count);
+        if (data.dishCount() < 20) {
+            data.dishCount(data.dishCount() + 1);
+            data.meal().priceCounter = +data.dishCount() * data.meal().price;
+
+            var total = 0;
+            self.foods().forEach(function(meal) {
+                total += meal.meal().priceCounter;
+            });
+
+            self.totalPrice(total);
+            self.selectedFood.push(new SeatReservation(self.availableMeals[event.target.id - 1]));
+        }
     }
-}
     this.resetClicks = function(data) {
         self.totalPrice(0);
     };
-}
+    }
 
 ko.applyBindings(new ReservationsViewModel());
